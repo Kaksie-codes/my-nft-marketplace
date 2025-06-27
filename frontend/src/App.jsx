fda5cf6630c89a6c58afd5e824b2ee883a8cb6ed
Nsikak Akpan <builtbykaksy@gmail.com>
2025-06-27 17:41:44 +0100

    uop

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/HomePage'
+import Header from './components/Header'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Header/>
+      <Routes>    
+       <Route path="/" element={<HomePage/>} />
+       <Route path="/about" element={<h1>About Page</h1>} />
+       <Route path="/contact" element={<h1>Contact Page</h1>} />
+       <Route path="/nft/:girl" element={<h1>NFT Details Page</h1>} />
+       <Route path="/create-nft" element={<h1>Create NFT Page</h1>} />
+       <Route path="/profile/rat" element={<h1>Profile Page</h1>} />
+       <Route path="/settings" element={<h1>Settings Page</h1>} />
+       <Route path="/login" element={<h1>Login Page</h1>} />
+       <Route path="/register" element={<h1>Register Page</h1>} />
+       <Route path="/404" element={<h1>404 Not Found</h1>} />
+       <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-red-500 text-5xl text-white'>Header</div>
+  )
+}
+
+export default Header
