318895470d54439bb61333669dd2856779ed1252
Nsikak Akpan <builtbykaksy@gmail.com>
2025-07-27 07:03:41 +0100

    basic layout completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,20 +8,7 @@ function App() {
 
   return (
     <div className='bg-primary'>
-      <Header/>
-      <Routes>    
-       <Route path="/" element={<HomePage/>} />
-       <Route path="/about" element={<h1>About Page</h1>} />
-       <Route path="/contact" element={<h1>Contact Page</h1>} />
-       <Route path="/nft/:girl" element={<h1>NFT Details Page</h1>} />
-       <Route path="/create-nft" element={<h1>Create NFT Page</h1>} />
-       <Route path="/profile/rat" element={<h1>Profile Page</h1>} />
-       <Route path="/settings" element={<h1>Settings Page</h1>} />
-       <Route path="/login" element={<h1>Login Page</h1>} />
-       <Route path="/register" element={<h1>Register Page</h1>} />
-       <Route path="/404" element={<h1>404 Not Found</h1>} />
-       <Route path="*" element={<h1>404 Not Found</h1>} />
-      </Routes>
+      
     </div>
   )
 }
