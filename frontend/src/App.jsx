76f946a1ec1c235e36124f9f9e2a1e244e16f26c
Nsikak Akpan <builtbykaksy@gmail.com>
2025-07-27 07:10:53 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import HomePage from './pages/HomePage'
-import Header from './components/Header'
 
 function App() {
   
 
   return (
     <div className='bg-primary'>
-      
+      rt
     </div>
   )
 }
