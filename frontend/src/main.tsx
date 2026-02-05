62ce764150d15583d9e0f746b2b96c972e485e6d
Nsikak Akpan <builtbykaksy@gmail.com>
2026-02-05 10:40:41 +0100

    updates

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@rainbow-me/rainbowkit/styles.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeProvider.tsx'
+import { WagmiProvider } from 'wagmi'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { wagmiConfig } from './lib/rainbowkitConfig.ts'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <WagmiProvider config={wagmiConfig}>
+          <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider>
+              <App />
+            </RainbowKitProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
